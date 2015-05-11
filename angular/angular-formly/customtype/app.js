0a1a34602ea37fdbb4ce2e05c53a16689b4ef978
zhentian-wan <zhentian@monkeyexperience.com>
2015-05-11 20:02:46 +0300

    csutom type

---
+++ angular/angular-formly/customtype/app.js
@@ -0,0 +1,34 @@
+(function() {
+
+    'use strict';
+
+    var app = angular.module('formlyExample', ['formly', 'formlyBootstrap']);
+
+    app.run(function(formlyConfig) {
+        formlyConfig.setType({
+            name: 'input',
+            templateUrl: 'input.html'
+        });
+    });
+
+    app.controller('MainCtrl', function MainCtrl() {
+        var vm = this;
+        vm.model = {};
+        vm.fields = [
+            {
+                type: 'input',
+                key: "firstName",
+                templateOptions: {
+                    label: 'Enter your first name',
+                    required: true
+                },
+                expressionProperties: {
+                    'templateOptions.disable': function($viewValue, $modelValue, scope) {
+                        return !scope.model.firstName;
+                    }
+                }
+            }
+        ];
+    });
+
+})();
